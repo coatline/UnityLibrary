584168ed85b82e774031575ef6c3f99bdc3d15a5
Bryler <bryler.castle@gmail.com>
2025-03-04 09:30:28 -0500

    Renamed SoundManager to SoundPlayer

--- Essentials/SoundPlayer.cs
+++ Essentials/SoundPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : Singleton<SoundManager>
+public class SoundPlayer : Singleton<SoundPlayer>
 {
     [SerializeField] AudioSource audioSourcePrefab;
 
